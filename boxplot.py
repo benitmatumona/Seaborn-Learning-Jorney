5409cb817ee55f61fa3fabf7e6125d2ecc51c06e
Benit Polvie <polviedivine@gmail.com>
2026-06-04 15:43:46 +0200

    Update boxplot.py

--- boxplot.py
+++ boxplot.py
@@ -15,12 +15,12 @@ def main():
         "performance_score \nremote_work_days \nsatisfaction"
     )
     topics = input("which topic(s) would you like the graph to displayed? ").strip().lower()
+    
     for topic in topics.split():
         try:
             sns.boxplot(x="age", y="salary", data=data, hue=topic)
         except Exception as e:
             print(f"Invalid input {topic}")
-
 
 if __name__ == "__main__":
     main()
